637e9a0444571efd72d6efb8f0923bb1d10ceb4b
dbouquin <dbouquin@gmail.com>
2023-06-29 11:38:29 -0400

    update html name in app

--- app.py
+++ app.py
@@ -1,41 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-from csv_transformer import modify_csv
+from flask import Flask, render_template, request, jsonify, redirect, url_for, send_from_directory
+from task_manager.task_manager import TaskManager
+import uuid
+import time
 import os
 
 app = Flask(__name__)
-app.config["UPLOADS_DIR"] = "uploads"
-app.config["EXPORTS_DIR"] = "exports"
+task_manager = TaskManager()
 
-@app.route("/", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        if "file" not in request.files:
-            return "No file found"
+@app.route('/')
+def index():
+    return render_template('upload.html')
 
-        file = request.files["file"]
+@app.route('/convert', methods=['POST'])
+def convert():
+    # Start a new task and get the task identifier
+    task_id = task_manager.start_task()
 
-        if file.filename == "":
-            return "File name is empty"
+    # Retrieve the uploaded file
+    file = request.files['file']
 
-        file_path = os.path.join(app.config["UPLOADS_DIR"], file.filename)
-        file.save(file_path)
+    # Process the file asynchronously in the task manager
+    task_manager.process_file(task_id, file)
 
-        # Modify the CSV file and get a new file path
-        modified_file_path = modify_csv(file_path)
-        modified_file_name = os.path.basename(modified_file_path)
+    # Return the task identifier to the client
+    return jsonify({'task_id': task_id})
 
-        return redirect(url_for("confirmation", filename=modified_file_name))
+@app.route('/progress/<task_id>')
+def progress(task_id):
+    # Get the progress and estimated time remaining for the task
+    progress, estimated_time = task_manager.get_task_progress(task_id)
 
-    return render_template("upload.html")
+    # Return the progress information to the client
+    return jsonify({'progress': progress, 'estimated_time': estimated_time})
 
-@app.route("/confirmation/<filename>")
-def confirmation(filename):
-    return render_template("confirmation.html", filename=filename)
-
-@app.route("/download/<filename>")
-def download(filename):
-    exports_dir = app.config["EXPORTS_DIR"]
-    return send_from_directory(exports_dir, filename, as_attachment=True)
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
